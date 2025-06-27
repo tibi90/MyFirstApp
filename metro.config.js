03ade8791796bc62542ac3e22621bd2c70ff8de5
tibi90 <tiziano.bizzozzero@gmail.com>
2025-06-27 08:41:24 +0200

    Fix module import error - update metro and typescript config

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,13 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resetCache: true,
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
