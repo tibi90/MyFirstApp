121ac53a0ec6530cb9f3645eee4b4d7cae4e3cfa
tibi90 <tiziano.bizzozzero@gmail.com>
2025-06-27 19:15:10 +0200

    Fix crash - remove all resource dependencies
    - Activity now creates UI programmatically
    - Removed theme from manifest
    - Removed styles.xml
    - Completely self-contained app with no external dependencies

--- android/app/src/main/java/com/myfirstapp/SimpleMainActivity.java
+++ android/app/src/main/java/com/myfirstapp/SimpleMainActivity.java
@@ -5,19 +5,24 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.view.Gravity;
 import android.graphics.Color;
+import android.widget.LinearLayout;
 
 public class SimpleMainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        // Create layout programmatically to avoid any resource dependencies
+        LinearLayout layout = new LinearLayout(this);
+        layout.setBackgroundColor(Color.WHITE);
+        layout.setGravity(Gravity.CENTER);
+        
         TextView textView = new TextView(this);
         textView.setText("Hello World!");
         textView.setTextSize(32);
-        textView.setGravity(Gravity.CENTER);
         textView.setTextColor(Color.BLACK);
-        textView.setBackgroundColor(Color.WHITE);
         
-        setContentView(textView);
+        layout.addView(textView);
+        setContentView(layout);
     }
 }
