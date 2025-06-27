32fa3263d2adde49f057216e635d222d1b70f456
tibi90 <tiziano.bizzozzero@gmail.com>
2025-06-27 19:31:48 +0200

    Fix visibility - explicitly set colors and layout
    - Set window background to white
    - Set text color to black
    - Set text size and center it
    - Set layout params to fill screen

--- android/app/src/main/java/com/myfirstapp/MainActivity.java
+++ android/app/src/main/java/com/myfirstapp/MainActivity.java
@@ -3,13 +3,25 @@ package com.myfirstapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Gravity;
+import android.graphics.Color;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Make sure window is visible
+        getWindow().setBackgroundDrawableResource(android.R.color.white);
+        
         TextView tv = new TextView(this);
         tv.setText("Hello World!");
+        tv.setTextSize(30);
+        tv.setTextColor(Color.BLACK);
+        tv.setGravity(Gravity.CENTER);
+        tv.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        
         setContentView(tv);
     }
 }
